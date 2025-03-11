014a571251774095db1e89541c9417498f8049ee
HonKelv <amuniikelvinokai5@gmail.com>
2025-03-11 13:05:22 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const navLinks = document.getElementById("nav-links");
+
+  menuIcon.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+});
 
 
 menuIcon.addEventListener('click', () => {
